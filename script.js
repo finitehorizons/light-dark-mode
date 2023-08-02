c18aa5cde3b27aa58b5dfa67347de1c24d1956d4
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-08-02 07:28:35 -0500

    Basic functionality done, need to be able to save to local storage

---
+++ script.js
@@ -0,0 +1,46 @@
+//Constants
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image-1");
+const image2 = document.getElementById("image-2");
+const image3 = document.getElementById("image-3");
+const textBox = document.getElementById("text-box");
+
+// Dark or Light images
+function imageMode(color) {
+    image1.src = `img/undraw_feeling_proud_${color}.svg`;
+    image2.src = `img/undraw_portfolio_website_${color}.svg`;
+    image3.src = `img/undraw_maker_launch_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    toggleIcon.children[0].textContent = "Dark Mode";
+    toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+    imageMode("dark");
+}
+
+function lightMode() {
+    nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    toggleIcon.children[0].textContent = "Light Mode";
+    toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+    imageMode("light");
+}
+
+// Theme Switcher
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        darkMode();
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        lightMode();
+    }
+}
+
+//Event Listeners
+toggleSwitch.addEventListener("change", switchTheme);
